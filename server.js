a5604e6063a2d2cca584d9324f795306423b74c9
Michael Williams <manicdesigner@gmail.com>
2020-03-25 18:47:28 -0400

    add authrouter to server

--- server.js
+++ server.js
@@ -1,8 +1,26 @@
 /** @format */
 
 const express = require('express');
+const cors = require('cors');
+const helmet = require('helmet');
+const authRouter = require('./auth/auth-router');
 const server = express();
-
+server.use(cors());
+server.use(helmet());
 server.use(express.json());
+
+server.get('/', (req, res) => {
+  res.send('API is up and running');
+});
+
+server.use('/api', authRouter);
+
+server.use((req, res) => {
+  res.status(404).json({ message: '404 page not found' });
+});
+
+server.use((err, req, res, next) => {
+  res.status(500).json({ message: 'internal server error' });
+});
 
 module.exports = server;
