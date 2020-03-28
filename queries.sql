81624a4bfdaf5d5bec4e0e3c90685576bcd0ac3d
Michael Williams <manicdesigner@gmail.com>
2020-03-28 12:50:03 -0400

    sql queries to plan knex queries off of.

---
+++ queries.sql
@@ -0,0 +1,22 @@
+--- unanswered questions
+Select DISTINCT(q."id"), q."title" as "title", q."category" as "category", q."attempt_tried" as "attempt_tried",
+q."comments" as "comments", 
+u."username" as "username"
+FROM "question_statuses" as qs
+ JOIN "questions" as q
+ON qs."question_id" = q."id"
+JOIN "users" as u
+ON  q."user_id" = u."id"
+WHERE qs."solved" = 0;
+
+--- answered questions
+
+Select DISTINCT(q."id"), q."title" as "title", q."category" as "category", q."attempt_tried" as "attempt_tried",
+q."comments" as "comments", 
+u."username" as "username"
+FROM "question_statuses" as qs
+ JOIN "questions" as q
+ON qs."question_id" = q."id"
+JOIN "users" as u
+ON  q."user_id" = u."id"
+WHERE qs."solved" = 1;
