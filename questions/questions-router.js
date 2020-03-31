72db38070dcc3de162579d9f6d6b19159fb78284
Michael Williams <manicdesigner@gmail.com>
2020-03-31 16:50:58 -0400

    comments on routes

--- questions/questions-router.js
+++ questions/questions-router.js
@@ -4,6 +4,12 @@ const restrict = require('../middleware/restrict');
 const QuestionModel = require('../questions/questions-model');
 const router = express.Router();
 
+/**
+ * @type GET /api/unanswered
+ * @description get all unanswered question data back w/ username who asked.
+ * @middleware restrict() => must be logged in to access.
+ * @errors 0 unanswered questions return a 400 'no questions to ask'
+ */
 router.get('/unanswered', restrict(), async (req, res, next) => {
   try {
     const unanswered = await QuestionModel.unansweredQuestions();
@@ -18,6 +24,12 @@ router.get('/unanswered', restrict(), async (req, res, next) => {
   }
 });
 
+/**
+ * @type Get /api/unanswered/:id
+ * @description get a specific unanswered question back
+ * @middleware restrict() => must be logged in to access.
+ * @errors none existent question id will return a 404
+ */
 router.get('/unanswered/:id', restrict(), async (req, res, next) => {
   const { id } = req.params;
   try {
