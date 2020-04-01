26c7fdbe9f34952f0b82fff44e14023ae26d89e8
Michael Williams <manicdesigner@gmail.com>
2020-04-01 02:18:28 -0400

    add user-dev as a role in restrict

--- middleware/restrictRoleQuestion.js
+++ middleware/restrictRoleQuestion.js
@@ -1,7 +1,7 @@
 
 function restrictRoleQuestion(user) {
     return async (req, res, next) => {
-        if(req.token && req.token.role === 'user') {
+        if(req.token && req.token.role === 'user' || req.token.role === 'user-dev') {
             next();
         } else {
             return res.status(403).json({ message: 'access not granted'})
