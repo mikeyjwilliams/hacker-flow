2a8681afc7cf6d4f2d7ab0475ed04e69cc57b62d
Michael Williams <manicdesigner@gmail.com>
2020-04-02 17:50:12 -0400

    built out restrictRoleAnswer middle ware restrict only dev's and user-devs to access this section of site.

---
+++ middleware/restrictRoleAnswer.js
@@ -0,0 +1,13 @@
+
+function restrictRoleAnswer(user) {
+    return async (req, res, next) => {
+        if(req.token && req.token.role === 'dev' ||
+         req.token.role === 'user-dev') {
+             next();
+         } else {
+             return res.status(403).json({ message: 'access not granted'});
+         }
+    }
+}
+
+module.exports = restrictRoleAnswer;
