f07f7828b558cab8a39d39be1664113128d0b4eb
Michael Williams <manicdesigner@gmail.com>
2020-04-03 15:18:28 -0400

    mv config file

---
+++ config/jest-runner-eslint.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    cliOptions: {
+        "ext": [".js", ".jsx"],
+        "env": "jest",
+        "ignorePattern: ["./server.js", "./index.js"],
+        "plugin": "prettier",
+        "reportUnusedDisableDirectives": true,
+        
+    }
+}
