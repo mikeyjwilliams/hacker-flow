9dcd25d987000b23b5f0a188e62776d4d30a1f47
Michael Williams <manicdesigner@gmail.com>
2020-04-03 20:06:49 -0400

    restrictRoleAnswer  middleware keeping only those with access to answer questions

--- middleware/restrictRoleAnswer.js
+++ middleware/restrictRoleAnswer.js
@@ -1,8 +1,8 @@
 
 function restrictRoleAnswer() {
     return async (req, res, next) => {
-        if(req.token && req.token.role === 'dev' ||
-         req.token.role === 'user-dev') {
+        if(req.token && (req.token.role === 'dev' ||
+         req.token.role === 'user-dev')) {
              next();
          } else {
              return res.status(403).json({ message: 'access not granted'});
