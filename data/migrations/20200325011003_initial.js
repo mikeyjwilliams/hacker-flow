6efe03fdbdcc553da477f6726b2a6f55eb4edf93
Michael Williams <manicdesigner@gmail.com>
2020-05-03 10:26:12 -0400

    update initial migration

--- data/migrations/20200325011003_initial.js
+++ data/migrations/20200325011003_initial.js
@@ -1,14 +1,28 @@
 /** @format */
 
 exports.up = async function (knex) {
+  await knex.schema.createTable('sign_ins', (tbl) => {
+    tbl.increments('id');
+    tbl.string('email', 165).notNullable().unique();
+    tbl.text('password').notNullable();
+  });
+
   await knex.schema.createTable('users', (tbl) => {
     tbl.increments('id');
     tbl.string('username', 165).notNullable().unique();
-    tbl.text('password').notNullable();
-    tbl.string('email', 165).notNullable().unique();
     tbl.string('first_name', 125).notNullable();
     tbl.string('last_name', 125).notNullable();
+  });
+
+  await knex.schema.createTable('roles', (tbl) => {
+    tbl.increments('id');
     tbl.string('role', 15).notNullable();
+    tbl
+      .integer('user_id')
+      .references('id')
+      .inTable('users')
+      .onUpdate('CASCADE')
+      .onDelete('CASCADE');
   });
 
   await knex.schema.createTable('questions', (tbl) => {
@@ -19,7 +33,12 @@ exports.up = async function (knex) {
     tbl.text('attempt_tried').nullable();
     tbl.text('comments').nullable();
     tbl.boolean('solved').notNullable().defaultTo('false');
-    tbl.integer('user_id').references('id').inTable('users');
+    tbl
+      .integer('user_id')
+      .references('id')
+      .inTable('users')
+      .onUpdate('CASCADE')
+      .onDelete('CASCADE');
   });
 
   await knex.schema.createTable('answers', (tbl) => {
@@ -28,13 +47,25 @@ exports.up = async function (knex) {
     tbl.text('solution').notNullable();
     tbl.text('comments').nullable();
     tbl.boolean('best_answer').notNullable().defaultTo('false');
-    tbl.integer('question_id').references('id').inTable('questions');
-    tbl.integer('dev_id').references('id').inTable('users');
+    tbl
+      .integer('question_id')
+      .references('id')
+      .inTable('questions')
+      .onUpdate('CASCADE')
+      .onDelete('CASCADE');
+    tbl
+      .integer('dev_id')
+      .references('id')
+      .inTable('users')
+      .onUpdate('CASCADE')
+      .onDelete('CASCADE');
   });
 };
 
 exports.down = async function (knex) {
   await knex.schema.dropTableIfExists('answers');
   await knex.schema.dropTableIfExists('questions');
+  await knex.schema.dropTableIfExists('roles');
   await knex.schema.dropTableIfExists('users');
+  await knex.schema.dropTableIfExists('sign_ins');
 };
