118b3ce2f0dd45e25a44c0115b1a0281c9e00e02
Michael Williams <manicdesigner@gmail.com>
2020-05-04 12:55:29 -0400

    reformat

--- auth/auth-model.js
+++ auth/auth-model.js
@@ -4,28 +4,28 @@ const db = require('../data/config');
 const bcrypt = require('bcryptjs');
 
 module.exports = {
-	findByPass,
-	findBy,
-	findById,
-	addUser
+  findByPass,
+  findBy,
+  findById,
+  addUser,
 };
 
 function findByPass(filter) {
-	return db('sign_ins').where(filter).select('email', 'password').first();
+  return db('sign_ins').where(filter).select('id', 'email', 'password').first();
 }
 
 function findById(id) {
-	return db('sign_ins').where({ id }).select('id', 'email').first();
+  return db('sign_ins').where({ id }).select('id', 'email').first();
 }
 
 function findBy(filter) {
-	return db('sign_ins').where(filter).select('id', 'email');
+  return db('sign_ins').where(filter).select('id', 'email');
 }
 
 async function addUser(user) {
-	user.password = await bcrypt.hash(user.password, 10);
+  user.password = await bcrypt.hash(user.password, 10);
 
-	const [id] = await db('sign_ins').insert(user);
+  const [id] = await db('sign_ins').insert(user);
 
-	return findById(id);
+  return findById(id);
 }
