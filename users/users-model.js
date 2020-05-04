60c0238d7462166115e03a68c109f2b39a67d015
Michael Williams <manicdesigner@gmail.com>
2020-05-03 23:18:38 -0400

    rm roles models from user model

--- users/users-model.js
+++ users/users-model.js
@@ -4,47 +4,38 @@ const db = require('../data/config');
 const bcrypt = require('bcryptjs');
 
 module.exports = {
-	getUsers,
-	findBy,
-	findById,
-	addUser,
-	addRole,
-	findByIdRole
-	//deleteUser,
+  getUsers,
+  findBy,
+  findById,
+  addUser,
+  // addRole,
+  // findByIdRole
+  //deleteUser,
 };
 
 function getUsers() {
-	return db('users').select('id', 'username', 'first_name', 'last_name');
+  return db('users').select('id', 'username', 'first_name', 'last_name');
 }
 
 function findBy(filter) {
-	return db('users')
-		.where(filter)
-		.select('id', 'username', 'email', 'first_name', 'last_name');
+  return db('users')
+    .where(filter)
+    .select('id', 'username', 'email', 'first_name', 'last_name');
 }
 
 function findById(id) {
-	return db('users')
-		.select('id', 'username', 'first_name', 'last_name')
-		.where({ id })
-		.first();
+  return db('users')
+    .select('id', 'username', 'first_name', 'last_name')
+    .where({ id })
+    .first();
 }
 
 async function addUser(user) {
-	const [id] = await db('users').insert(user);
+  const [id] = await db('users').insert(user);
 
-	return findById(id);
+  return findById(id);
 }
 
-async function addRole(role = 'user-dev') {
-	const [id] = await db('roles').insert(role);
-
-	return findByIdRole(id);
-}
-
-function findByIdRole(id) {
-	return db('roles').where({ id }).select('id', 'role').first();
-}
 //!! Foreign key constraints this.
 // function deleteUser(id) {
 //   return db('users')
