d9bfc4221d743e8ce4e859005f018a3d85f069f9
Michael Williams <manicdesigner@gmail.com>
2020-05-03 23:56:13 -0400

    async middleware

--- middleware/registerVerify.js
+++ middleware/registerVerify.js
@@ -1,40 +1,46 @@
+/** @format */
+
 const authModel = require('../auth/auth-model');
 
 function registerVerify() {
-    return (req, res, next) => {
-        const emailAddressToLong = 165;
-        const emailAddressToShort = 3;
-        const passwordToShort = 3;
-        const { email, password } = req.body;
-    if (!email) {
-      return res.status(400).json({ message: 'email is required' });
-    } else if (email.length > emailAddressToLong) {
-      return res.status(400).json({
-        message: `email is longer than ${emailAddressToLong} characters... sorry too long`,
-      });
-    } else if (email.length < emailAddressToShort) {
-      return res.status(400).json({ message: 'email is too short' });
-    } else if (!email.includes('@')) {
-      return res
-        .status(400)
-        .json({ message: 'an email address requires an @ sign' });
-    }
-    if (!password) {
-      return res.status(400).json({ message: 'password is required' });
-    }
-    if (password.length < passwordToShort) {
-      return res.status(400).json({
-        message: `password too short ${passwordToShort} characters or longer`,
-      });
-    }
-    const emailCheck = await authModel.findBy({ email }).first();
+  return async (req, res, next) => {
+    try {
+      const emailAddressToLong = 165;
+      const emailAddressToShort = 3;
+      const passwordToShort = 3;
+      const { email, password } = req.body;
+      if (!email) {
+        return res.status(400).json({ message: 'email is required' });
+      } else if (email.length > emailAddressToLong) {
+        return res.status(400).json({
+          message: `email is longer than ${emailAddressToLong} characters... sorry too long`,
+        });
+      } else if (email.length < emailAddressToShort) {
+        return res.status(400).json({ message: 'email is too short' });
+      } else if (!email.includes('@')) {
+        return res
+          .status(400)
+          .json({ message: 'an email address requires an @ sign' });
+      }
+      if (!password) {
+        return res.status(400).json({ message: 'password is required' });
+      }
+      if (password.length < passwordToShort) {
+        return res.status(400).json({
+          message: `password too short ${passwordToShort} characters or longer`,
+        });
+      }
+      const emailCheck = await authModel.findBy({ email }).first();
 
-    if (emailCheck) {
-      return res
-        .status(409)
-        .json({ message: `email ${emailCheck.email} is not available.` });
+      if (emailCheck) {
+        return res
+          .status(409)
+          .json({ message: `email ${emailCheck.email} is not available.` });
+      }
+      next();
+    } catch (err) {
+      next(err);
     }
-    next();
-    }
+  };
 }
 module.exports = registerVerify;
