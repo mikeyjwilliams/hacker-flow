b2d3b30f5147ec3a9846d860f9713e548505f8f3
Michael Williams <manicdesigner@gmail.com>
2020-05-04 12:55:17 -0400

    mod genToken some

--- auth/genToken.js
+++ auth/genToken.js
@@ -1,3 +1,5 @@
+/** @format */
+
 const jwt = require('jsonwebtoken');
 const secret = require('../Secret/secret');
 /**
@@ -5,15 +7,15 @@ const secret = require('../Secret/secret');
  * @param {*} user
  */
 function genToken(user) {
-	const payload = {
-		userId: user.id,
-		username: user.username,
-		role: user.role
-	};
-	// const options = {
-	// 	//!! expiresIn: '25d' these tokens never expire!!!
-	// };
-	return jwt.sign(payload, secret.jwtSecret);
+  const payload = {
+    userId: user.id,
+    username: user.username,
+    role: user.role || 'user-dev',
+  };
+  // const options = {
+  // 	//!! expiresIn: '25d' these tokens never expire!!!
+  // };
+  return jwt.sign(payload, secret.jwtSecret /*, options*/);
 }
 
 module.exports = genToken;
