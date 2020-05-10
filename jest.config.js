46d3ad55ce9586606458bef5da2eef6555e1a33f
Mikey Williams <manicdesigner@gmail.com>
2020-05-10 14:57:20 -0400

    reformat

--- jest.config.js
+++ jest.config.js
@@ -1,53 +1,53 @@
 /** @format */
 
 module.exports = {
-  collectCoverage: false,
-  verbose: true,
-  browser: false,
-  errorOnDeprecated: true,
-  moduleFileExtensions: ['js', 'json', 'jsx', 'ts', 'tsx', 'node'],
-  coverageDirectory: './coverage',
-  collectCoverageFrom: [
-    // coverage for these files.
-    '**/*.{js,jsx}',
-    '!**/node_modules/**',
-    '!**/vendor/**',
-    '!**/database/**',
-    '!**/tests/**',
-    '!**/spec/**',
-    '!**/*.spec.{js,jsx}',
-    '!**/coverage/**',
-  ],
+	collectCoverage: false,
+	verbose: true,
+	browser: false,
+	errorOnDeprecated: true,
+	moduleFileExtensions: ['js', 'json', 'jsx', 'ts', 'tsx', 'node'],
+	coverageDirectory: './coverage',
+	collectCoverageFrom: [
+		// coverage for these files.
+		'**/*.{js,jsx}',
+		'!**/node_modules/**',
+		'!**/vendor/**',
+		'!**/database/**',
+		'!**/tests/**',
+		'!**/spec/**',
+		'!**/*.spec.{js,jsx}',
+		'!**/coverage/**'
+	],
 
-  coverageReporters: [
-    'json',
-    'lcov',
-    'text',
-    'clover',
-    'text-summary',
-    'json-summary',
-  ],
-  coverageThreshold: {
-    global: {
-      branches: 50,
-      functions: 50,
-      lines: 50,
-      statements: 50,
-    },
-    projects: [
-      {
-        displayName: 'test',
-      }, //,
-      // {
-      //   displayName: 'jest-runner-eslint',
-      //   displayName: 'lint',
-      //   testMatch: ['<rootDir>/**/*.js']
-      // },
-      // {
-      //   displayName: 'jest-runner-prettier',
-      //   displayName: 'prettier',
-      //   testMatch: ['<rootDir>/**/*.js']
-      // }
-    ],
-  },
+	coverageReporters: [
+		'json',
+		'lcov',
+		'text',
+		'clover',
+		'text-summary',
+		'json-summary'
+	],
+	coverageThreshold: {
+		global: {
+			branches: 50,
+			functions: 50,
+			lines: 50,
+			statements: 50
+		},
+		projects: [
+			{
+				displayName: 'test'
+			} //,
+			// {
+			//   displayName: 'jest-runner-eslint',
+			//   displayName: 'lint',
+			//   testMatch: ['<rootDir>/**/*.js']
+			// },
+			// {
+			//   displayName: 'jest-runner-prettier',
+			//   displayName: 'prettier',
+			//   testMatch: ['<rootDir>/**/*.js']
+			// }
+		]
+	}
 };
