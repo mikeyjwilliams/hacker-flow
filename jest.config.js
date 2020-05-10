7a71a770a0b30fe2c966f7e250a24a255c48c403
Michael Williams <manicdesigner@gmail.com>
2020-05-09 21:54:46 -0400

    coverage is removed will be created on CI test

--- jest.config.js
+++ jest.config.js
@@ -9,78 +9,45 @@ module.exports = {
   coverageDirectory: './coverage',
   collectCoverageFrom: [
     // coverage for these files.
-    '<rootDir>/auth/**/*.js',
-    '<rootDir>/answers/**/*.js',
-    '<rootDir>/questions/**/*.js',
-    '<rootDir>/users/**/*.js',
-    // spec tests no coverage
-    '!<rootDir>/auth/**/*.spec.js',
-    '!<rootDir>/answers/**/*.spec.js',
-    '!<rootDir>/questions/**/*.spec.js',
-    '!<rootDir>/users/**/*.spec.js',
-    '!<rootDir>/Secret/**/*.js',
-    '!<rootDir>/middleware/**/*.js',
-    // individual files no coverage
-    '!<rootDir>/index.js',
-    '!<rootDir>/server.js',
-    '!<rootDir>/knexfile.js',
-    '!<rootDir>/index.js'
+    '**/*.{js,jsx}',
+    '!**/node_modules/**',
+    '!**/vendor/**',
+    '!**/database/**',
+    '!**/tests/**',
+    '!**/spec/**',
+    '!**/*.spec.{js,jsx}',
+    '!**/coverage/**',
   ],
-  coveragePathIgnorePatterns: [
-    '/node_modules/',
-    '/Secret/',
-    '/middleware/',
-    '/.github/',
-    '/config/',
-    '/data/'
-  ],
+
   coverageReporters: [
     'json',
     'lcov',
     'text',
     'clover',
     'text-summary',
-    'json-summary'
+    'json-summary',
   ],
   coverageThreshold: {
     global: {
       branches: 50,
       functions: 50,
       lines: 50,
-      statements: -20
+      statements: 50,
     },
-    './auth/': {
-      branches: 75,
-      functions: 90,
-      lines: 65,
-      statements: -20
-    },
-    './answers/': {
-      branches: 65,
-      functions: 75
-    },
-    './questions/': {
-      branches: 60,
-      functions: 70
-    },
-    './users/': {
-      branches: 60,
-      functions: 75
-    }
+    projects: [
+      {
+        displayName: 'test',
+      }, //,
+      // {
+      //   displayName: 'jest-runner-eslint',
+      //   displayName: 'lint',
+      //   testMatch: ['<rootDir>/**/*.js']
+      // },
+      // {
+      //   displayName: 'jest-runner-prettier',
+      //   displayName: 'prettier',
+      //   testMatch: ['<rootDir>/**/*.js']
+      // }
+    ],
   },
-  projects: [
-    {
-      displayName: 'test'
-    } //,
-    // {
-    //   displayName: 'jest-runner-eslint',
-    //   displayName: 'lint',
-    //   testMatch: ['<rootDir>/**/*.js']
-    // },
-    // {
-    //   displayName: 'jest-runner-prettier',
-    //   displayName: 'prettier',
-    //   testMatch: ['<rootDir>/**/*.js']
-    // }
-  ]
 };
