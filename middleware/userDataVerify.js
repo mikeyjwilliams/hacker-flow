5a1c3095010fa6d5546191cac2b875a7ff3874d2
Michael Williams <manicdesigner@gmail.com>
2020-05-10 16:48:11 -0400

    verify altered user_id for const to use other somewhere else

--- middleware/userDataVerify.js
+++ middleware/userDataVerify.js
@@ -1,32 +1,35 @@
 /**
- *! requires restrict.js middleware
- *! in front of this middlware to work correctly
- *! and to be secure.
+ * ! requires restrict.js middleware
+ * ! in front of this middlware to work correctly
+ * ! and to be secure.
+ *
+ * @format
  */
+
 const userModel = require('../users/users-model');
 
 function userDataVerify() {
-	return async (req, res, next) => {
-		try {
-			const userId = req.token.userId;
-			const userData = await userModel.getUser(userId);
+  return async (req, res, next) => {
+    try {
+      const user_id = req.token.userId;
+      const userData = await userModel.getUser(user_id);
 
-			if (userData.user_id === undefined) {
-				res.redirect('/api/usersetup');
-			}
+      if (userData.user_id === undefined) {
+        res.redirect('/api/usersetup');
+      }
 
-			req.userId = userData.user_id;
-			req.username = userData.username;
-			req.firstName = userData.first_name;
-			req.lastName = userData.last_name;
-			req.mainId = userData.main_id;
+      req.userId = userData.user_id;
+      req.username = userData.username;
+      req.firstName = userData.first_name;
+      req.lastName = userData.last_name;
+      req.mainId = userData.main_id;
 
-			next();
-		} catch (err) {
-			console.log(err);
-			next(err);
-		}
-	};
+      next();
+    } catch (err) {
+      console.log(err);
+      next(err);
+    }
+  };
 }
 
 module.exports = userDataVerify;
